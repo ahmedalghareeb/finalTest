8704a11378ce8a09b5c543dfb5436b0871595100
Ahmed Ghareeb <a.s.ghareeb@gmail.com>
2020-11-19 19:44:21 -0500

    final test first commit

---
+++ FinalTest1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FinalTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[2];
+
+            Adult adult = new Adult("Adult_first_name", "Adult_last_name ");
+            Child child = new Child("Child_first_name", "Child_last_name ");
+            persons[0] = adult;
+            persons[1] = child;
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.toString());
+            }
+
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string toString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    public class Adult : Person
+    {
+        public Adult(string firstName, string lastName) : base(firstName, lastName)
+        {
+
+        }
+    }
+
+    public class Child : Person
+    {
+        public Child(string firstName, string lastName) : base(firstName, lastName)
+        {
+
+        }
+    }
+
+
+}
